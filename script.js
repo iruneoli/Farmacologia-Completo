b8de56beb9d4b8a73a7ca71544d08e1a0000b795
iruneoli <faschatgpt@gmail.com>
2026-04-23 14:30:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function selectGroup(group) {
-  alert("Has seleccionado: " + group);
+  const homeScreen = document.getElementById("home-screen");
+  const digestivoScreen = document.getElementById("digestivo-screen");
+
+  if (group === "digestivo") {
+    homeScreen.classList.add("hidden");
+    digestivoScreen.classList.remove("hidden");
+  } else {
+    alert("De momento solo está programado ATC Digestivo.");
+  }
+}
+
+function goHome() {
+  const homeScreen = document.getElementById("home-screen");
+  const digestivoScreen = document.getElementById("digestivo-screen");
+
+  digestivoScreen.classList.add("hidden");
+  homeScreen.classList.remove("hidden");
+}
+
+function selectActivity(activity) {
+  alert("Has seleccionado la actividad: " + activity);
 }
