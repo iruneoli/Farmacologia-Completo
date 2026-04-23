6a77089d3ebc8a446e52f45593188e821470615d
iruneoli <faschatgpt@gmail.com>
2026-04-23 15:10:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,7 @@ function goHome() {
 
 function selectActivity(activity) {
   if (activity === "definiciones") {
-    document.getElementById("digestivo-screen").classList.add("hidden");
-    document.getElementById("definiciones-screen").classList.remove("hidden");
+    loadDefinitions(); 
   } else if (activity === "repaso-fallos") {
     alert("Repaso de fallos aún no implementado.");
   } else {
@@ -32,4 +31,38 @@ function selectActivity(activity) {
 function backToActivities() {
   document.getElementById("definiciones-screen").classList.add("hidden");
   document.getElementById("digestivo-screen").classList.remove("hidden");
+}
+
+async function loadDefinitions() {
+  document.getElementById("digestivo-screen").classList.add("hidden");
+  document.getElementById("definiciones-screen").classList.remove("hidden");
+
+  const response = await fetch("data/atc-digestivo/questions/definiciones.json");
+  const data = await response.json();
+
+  const question = data[0]; // primera pregunta
+
+  renderMatch(question);
+}
+
+function renderMatch(question) {
+  const leftCol = document.getElementById("left-column");
+  const rightCol = document.getElementById("right-column");
+
+  leftCol.innerHTML = "";
+  rightCol.innerHTML = "";
+
+  question.leftItems.forEach(item => {
+    const div = document.createElement("div");
+    div.className = "match-item";
+    div.innerText = item;
+    leftCol.appendChild(div);
+  });
+
+  question.rightItems.forEach(item => {
+    const div = document.createElement("div");
+    div.className = "match-item";
+    div.innerText = item;
+    rightCol.appendChild(div);
+  });
 }
